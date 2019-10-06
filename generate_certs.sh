c6fc6f001a122db3f24501e7ae7f1007bb1abc1d
dangelox <fernando.dilesh@gmail.com>
2019-10-06 15:06:26 +0000

    updated certs script

--- generate_certs.sh
+++ generate_certs.sh
@@ -21,13 +21,13 @@ echo -e "\n${YELLOW}Generate CA key${NC}"
 openssl genrsa -passout pass:1111 -des3 -out ca.key 4096
 
 echo -e "\n${YELLOW}Generate CA certificate${NC}"
-openssl req -passin pass:1111 -new -x509 -days 365 -key ca.key -out ca.crt -subj  "/C=US/ST=KS/L=Lenexa/O=LeggettandPlatt/OU=ConsumerProducts/CN=LenexaR&D"
+openssl req -passin pass:1111 -new -x509 -days 365 -key ca.key -out ca.crt -subj  "/C=US/ST=KS/L=Lenexa/O=LeggettandPlatt/OU=ConsumerProducts/CN=Lenexa R&D CA"
 
 echo -e "\n${YELLOW}Generate server key${NC}"
 openssl genrsa -passout pass:1111 -des3 -out server.key 4096
 
 echo -e "\n${YELLOW}Generate server signing request${NC}"
-openssl req -passin pass:1111 -new -key server.key -out server.csr -subj  "/C=US/ST=KS/L=Lenexa/O=LeggettandPlatt/OU=ConsumerProducts/CN=bigdata"
+openssl req -passin pass:1111 -new -key server.key -out server.csr -subj  "/C=US/ST=KS/L=Lenexa/O=LeggettandPlatt/OU=ConsumerProducts/CN=localhost"
 
 echo -e "\n${YELLOW}Self-sign server certificate${NC}"
 openssl x509 -req -passin pass:1111 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
@@ -39,7 +39,7 @@ echo -e "\n${YELLOW}Generate client key"
 openssl genrsa -passout pass:1111 -des3 -out client.key 4096
 
 echo -e "\n${YELLOW}Generate client signing request${NC}"
-openssl req -passin pass:1111 -new -key client.key -out client.csr -subj  "/C=US/ST=KS/L=Lenexa/O=LeggettandPlatt/OU=ConsumerProducts/CN=k8s"
+openssl req -passin pass:1111 -new -key client.key -out client.csr -subj  "/C=US/ST=KS/L=Lenexa/O=LeggettandPlatt/OU=ConsumerProducts/CN=localhost"
 
 echo -e "\n${YELLOW}Self-sign client certificate${NC}"
 openssl x509 -passin pass:1111 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
